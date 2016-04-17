dddda8c4c4a7f966eb45296005c02092a97896a2
Jon Perkins <jjperk@gmail.com>
2016-04-17 19:54:05 +0000

    Finish adding associations and add user/comment to database

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true, length: { maximum: 1000 }
   belongs_to :user
+  has_many :comments
 end
